cc333c35a0294dc746773629a2ab95790d5f1261
aerick-321 <158105735+aerick-321@users.noreply.github.com>
2024-11-14 14:12:53 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,10 +1,19 @@
+
+function ifsorted(array){
+    for(let i = 0; i < array.length-1; i++){
+        if (array[i] > array[i +1]{
+            permutationSort(array);
+        }
+    }
+}
+
 function permutationSort(a) {
     if (a.length<=1) {
         return 1;
     }
     else{
         let permArr = [];
-        for(let i = 0; c < a.length; i++){
+        for(let i = 0; i < a.length; i++){
             
         }
         
