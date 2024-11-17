b2ebf4be532cf35f00cfc47dfbd9aa0f761a8e50
aerick-321 <158105735+aerick-321@users.noreply.github.com>
2024-11-17 16:24:56 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,4 +1,3 @@
-
 function ifsorted(array){
     for(let i = 0; i < array.length-1; i++){
         if (array[i] > array[i +1]){
@@ -9,16 +8,22 @@ function ifsorted(array){
 }
 
 function permutationSort(a, num) {
+    num = 0;
     if (a.length<=1) {
+        if (isSorted(a)) { 
+            return 1;
+        }
         return 1;
     }
-    else{
-        let count = 0;
-        for(let i = num; i < a.length; i++){
-             [a[i], a[num]] = [a[num], a[i]];
-            permutationSort(a, num +1);
-            [a[i], a[num]] = [a[num], a[i]];
-            count++;
+    let count = 0;
+    for(let i = num; i < a.length; i++){
+        [a[i], a[num]] = [a[num], a[i]];
+        count += permutationSort(a, num + 1);
+        [a[i], a[num]] = [a[num], a[i]];
+        
+        if (isSorted(a)) {
+            break;
         }
     }
+    return count;
 }
